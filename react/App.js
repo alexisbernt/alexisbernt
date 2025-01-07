7b8a28bea3b3122c0d87b9fc156bdc6b8c14de98
Alexis Bernthal <129408149+alexisbernt@users.noreply.github.com>
2025-01-06 20:13:15 -0600

    Link in App

--- react/App.js
+++ react/App.js
@@ -1,11 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"; // use Link for handling routing in React 
+// Link allows for users to navigate between each page without reloading the application each time
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Bright Green Header</header>
+      <header className="App-header">
+      TheraText
+      <div className="greeting">Hello valued individual</div>
+      </header>
       <div className="App-body">
         <Link to="/page1">
           <button className="nav-button">Employee Portal</button>
