de2f4760596ab0d83c8e4ba89ca116cc01f9f00a
Alexis Bernthal <129408149+alexisbernt@users.noreply.github.com>
2025-01-30 16:05:35 -0600

    Add files via upload

---
+++ python/python_basics/python_starter.py
@@ -0,0 +1,12 @@
+# a function
+def function(parametera, parameterb):
+    #this is a while loop
+    while parametera != 0:
+        print(parameterb - 1)
+        parameter - 1
+        function(parametera, parameterb)
+
+parameter = 5
+parameter2 = 13
+
+function(parameter, parameter2)
