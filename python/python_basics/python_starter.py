87c8347b125acda36ad1caa8c9d55470b907578f
Alexis Bernthal <129408149+alexisbernt@users.noreply.github.com>
2025-01-31 21:17:42 -0600

    Add files via upload

--- python/python_basics/python_starter.py
+++ python/python_basics/python_starter.py
@@ -1,10 +1,12 @@
 # a function
 def function(parametera, parameterb):
     #this is a while loop
-    while parametera != 0:
-        print(parameterb - 1)
-        parameter - 1
-        function(parametera, parameterb)
+    while parameterb != 0:
+        parameterc = parameterb - 1
+        print(parameterc)  # Print the modified value of parameterb
+        parametera -= 1  # Correctly decrement parametera
+        parameterb -= 1
+        function(parametera, parameterb)  # Recursive call with updated value
 
 parameter = 5
 parameter2 = 13
