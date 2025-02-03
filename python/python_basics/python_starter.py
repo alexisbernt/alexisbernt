3b78ee18c1b995bc00369f5f197bf0fb4fbf95ee
Alexis Bernthal <129408149+alexisbernt@users.noreply.github.com>
2025-02-03 11:12:53 -0600

    python starter recursion

--- python/python_basics/python_starter.py
+++ python/python_basics/python_starter.py
@@ -3,12 +3,20 @@ def function(parametera, parameterb):
     #this is a while loop
     while parameterb != 0:
         parameterc = parameterb - 1
-        print(parameterc)  # Print the modified value of parameterb
-        parametera -= 1  # Correctly decrement parametera
+        print(parameterc)  # Print the modified value of parameterb (now parameterc)
         parameterb -= 1
         function(parametera, parameterb)  # Recursive call with updated value
 
-parameter = 5
+def function(parameterb):
+    if parameterb == 0:
+        return  # Base case to stop recursion
+    print(parameterb - 1)
+    function(parameterb - 1)  # Reduce parameterb on each call
+
+parameter1 = 5
 parameter2 = 13
 
-function(parameter, parameter2)
+# function(parameter1, parameter2)
+function(parameter2)
+
+# Python built-in functions: https://www.w3schools.com/python/python_ref_functions.asp
