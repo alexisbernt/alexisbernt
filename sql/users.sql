db6ce2c8f47821c305f8ab4121f9cacaac37ec70
Alexis Bernthal <129408149+alexisbernt@users.noreply.github.com>
2025-02-26 11:40:29 -0600

    Create users.sql

---
+++ sql/users.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users
+(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL
+  ); 
+
+To insert records into SQL
+INSET INTO users (name)
+VALUES 
+
+You can drop columns 
