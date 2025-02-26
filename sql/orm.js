2f917be11ec3351ed889d6d050ea1dfdadc8242b
Alexis Bernthal <129408149+alexisbernt@users.noreply.github.com>
2025-02-26 12:02:49 -0600

    Create orm.js

---
+++ sql/orm.js
@@ -0,0 +1,4 @@
+// Orm is an Orbject Relational Management System 
+// you are able to tell the data how it is related -- ORM will group the data together 
+
+// ORM is generic -- can generate methods for your SQL script 
